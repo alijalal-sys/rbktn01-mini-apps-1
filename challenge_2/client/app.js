c544da8266d676483f9145e37462aa365f90f338
alijalal-sys <ajmabilal@gmail.com>
2019-12-28 18:45:37 +0100

    not yet!

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,27 @@
+var newStr = '';
+var convertToCSV = function (obj) {
+    var recievedObj = JSON.parse(Object.values(obj));
+    var values = Object.values(recievedObj)
 
+    var arrKeys = Object.keys(recievedObj);
+    arrKeys.pop();
+    
+    
+    for (var i = 0; i < arrKeys.length; i++) {
+        newStr += arrKeys[i] + ', ';
+        if (i === arrKeys.length - 1) {
+            newStr += '\n';
+        }
+    }
 
+    for (var i = 0; i < values.length; i++) {
+        if (typeof values[i] === 'object') {
+            // TODO: Call a function
+        }
+
+        newStr += values[i] + ', '; 
+        if (i === values.length - 1) {
+            newStr += '\n';
+        }
+    }
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const port = 3000;
+
+app.use(express.static('client'))
+app.use(bodyParser())
 
 
+app.get('/', function (req, res) {
+    res.render('index', {});
+});
+
+app.post('/home', (req, res, next) => {
+    var recievedObj = JSON.parse(Object.values(req.body));
+    // console.log(recievedObj);
+    var values = Object.values(recievedObj)
+
+    var objKeys = Object.keys(recievedObj);
+    objKeys.pop();
+    var newStr = '';
+    for (var i = 0; i < values.length; i++) {
+        if (typeof values[i] === 'object') {
+            newStr += '\n'
+            for (var key in values[i]) {
+                if (typeof values[i][key] === 'object') {
+                    for (var element in values[i][key]) {
+                        if (typeof values[i][key][element] === 'object') {
+                            for (var obj in values[i][key][element]) {
+                                newStr += values[i][key][element][obj] + ', ';
+                            }
+                        }
+                        console.log(element)
+                        newStr += values[i][key][element] + ', ';
+                    }
+                    newStr += '\n';
+                }
+            }
+        }
+        newStr += values[i] + ', ';
+    }
+    console.log(newStr)
+    
+    
+    // res.end(JSON.stringify(array));
+})
+
+
+app.listen(port, () => {
+    console.log(`CSV Report Generator listening on Port ${port}`)
+})

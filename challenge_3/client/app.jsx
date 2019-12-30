89d8f6a9a0ea08e3e403df6080f37c97db432dc9
alijalal-sys <ajmabilal@gmail.com>
2019-12-30 19:33:15 +0100

    little bit of everything :)

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,69 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forms: [
+        <Form1 />,
+        <Form2 />,
+        <Form3 />
+      ],
+      idx: 0
+    }
+  };
 
+  handleClick = () => {
+    this.setState({
+      idx: this.state.idx + 1
+    });
+  };
 
+  render() {
+    return (
+      <div>
+        {this.state.forms[this.state.idx]}
+        <input onClick={this.handleClick} type='submit' name='next'/>
+      </div>
+    )
+  }
+}
+
+var Form1 = () => (
+  <div>
+    <form id='formElem' action="/" method="post">
+      Name: <input type="text" /> <br/>
+      E-mail: <input type="email" name="email" id="email"/> <br/>
+      Password: <input type="password" name="password" id="password"/> <br/>
+      <input type="submit" value="send"/>
+    </form>
+  </div>
+)
+
+var Form2 = () => (
+  <div>
+    <form action="/" method="post">
+      Address: <input type="text" name="address" /> <br/>
+      Address2: <input type="text" name="address2" /> <br/>
+      City: <input type="text" name="city" /> <br/>
+      State: <input type="text" name='state'/> <br/>
+      Zip-Code: <input type="number" name="zipCode"/> <br/>
+      <input type="submit" value="send"/>
+    </form>
+  </div>
+)
+
+var Form3 = () => (
+  <div>
+    <form action="/" method="post">
+      Credit Card: <input type="text" name="creditCard"/> <br/>
+      Exp-Date: <input type="date" name="expired" /> <br/>
+      CCV: <input type="number" name="ccv" /> <br/>
+      Zip-Code: <input type="number" name="zipCode" /> <br/>
+      <input type="submit" value="send"/>
+    </form>
+  </div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 3000;
 
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/app-3', { useNewUrlParser: true });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
+var infoSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    address: String,
+    address2: String,
+    city: String,
+    state: String,
+    zipCode: Number,
+    creditCard: Number,
+    expDate: Number,
+    ccv: Number,
+    zipCode: Number
+});
+
+var User = mongoose.model('User', infoSchema);
+
+app.use(express.static('public'));
+app.use(bodyParser ());
+
+app.post('/', (req, res, next) => {
+    var user = new User(req.body);
+    console.log(user)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on ${PORT}`)
+})
